bffaa24e9288982c55f0aa832bffe0422b27f61f
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-05-19 16:56:13 -0400

    Initial Commit

---
+++ XsAndOs/XsAndOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XsAndOsApp.swift
+//  XsAndOs
+//
+//  Created by Leonard McCook-Carr on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct XsAndOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
