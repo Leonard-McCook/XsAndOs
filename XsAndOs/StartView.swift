5f890749fdb7efb10057f42beac3180f64369dff
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-05-21 18:53:20 -0400

    Rename Content and XsAndOs views to StartView and AppEntry

--- XsAndOs/StartView.swift
+++ XsAndOs/StartView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct StartView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,8 +19,9 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct
+StartView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        StartView()
     }
 }
