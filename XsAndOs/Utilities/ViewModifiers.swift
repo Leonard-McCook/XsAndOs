1b26af96b7508016b0025831122095c41c82a146
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-05-25 17:42:35 -0400

    Create NavStackContainer struct in the ViewModifiers file

--- XsAndOs/Utilities/ViewModifiers.swift
+++ XsAndOs/Utilities/ViewModifiers.swift
@@ -6,3 +6,25 @@
 //
 
 import SwiftUI
+
+struct NavStackContainer: ViewModifier {
+    func body(content: Content) -> some View {
+        if #available(iOS 16, *) {
+            NavigationStack {
+                content
+            }
+        } else {
+            NavigationView {
+                content
+            }
+            .navigationViewStyle(.stack)
+        }
+    }
+}
+
+extension View {
+    public func inNavigationStack() -> some View {
+        return self.modifier(NavStackContainer())
+    }
+}
+ 
